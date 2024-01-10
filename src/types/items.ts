f219b8306cfdc3bee03486fa0623d944ed3a29fd
kiramDZ <hilmi0@mhs.unsyiah.ac.id>
2024-01-10 15:21:18 +0700

    data type for card

---
+++ src/types/items.ts
@@ -0,0 +1,10 @@
+export type Items = {
+    id: number;
+    img: string; //karena yang diterima itu link/url
+    user: string;
+    title: string;
+    location: string;
+    total: number;
+    btn: string;
+    btnLink: string;
+}
